222d0b1c305a2f7e162ba0b61056ba42fd38f72f
Donato Costa <donatocosta71@gmail.com>
2025-06-14 14:43:27 -0300

    camb

--- apps/reserva/tests/test_reserva.py
+++ apps/reserva/tests/test_reserva.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_django.fixtures import client
 from rest_framework import status
 from django.urls import reverse
 from datetime import date, timedelta
@@ -8,7 +9,7 @@ from .fixtures_turno import get_turno
 
 
 @pytest.mark.django_db
-def test_api_crear_reserva(get_authenticated_client, get_cancha, get_turno):
+def test_crear_reserva(get_authenticated_client, get_cancha, get_turno):
     client = get_authenticated_client
     cancha = get_cancha
     turno = get_turno
@@ -24,3 +25,28 @@ def test_api_crear_reserva(get_authenticated_client, get_cancha, get_turno):
     assert response.status_code == status.HTTP_201_CREATED
     assert response.data["cancha"] == get_cancha.id
     assert response.data["turno"] == get_turno.id
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("fecha_invalida, expected_error", [
+    (str(date.today()), "No se permiten reservas para el mismo día.", ),  # No permite reservas para hoy
+    (str(date.today() - timedelta(days=1)), "La fecha de la reserva no puede ser anterior a la fecha actual."),  # No permite fechas pasadas
+])
+def test_crear_reserva_fecha_invalida(get_authenticated_client, get_cancha, get_turno, fecha_invalida, expected_error):
+    client = get_authenticated_client
+
+    data = {
+        "fecha": fecha_invalida,
+        "cancha": get_cancha.id,
+        "turno": get_turno.id
+    }
+
+    response = client.post("/api/reserva/", data=data, format='json')
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert expected_error.lower() in str(response.data["fecha"][0]).lower()
+
+def test_crear_reserva_cancha_ocupada(get_authenticated_client, get_cancha, get_turno, get_reserva):
+    client = get_authenticated_client
+    cancha = get_cancha
+    turno = get_turno
+    reserva = get_reserva
